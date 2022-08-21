b2fefeeef26d8dd32197ada5e338d7fdd397fbed
marioBytes <mario.bytes92@gmail.com>
2022-08-20 21:09:37 -0600

    Install Axios to fetch users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
+import UserInfoCard from './components/UserInfoCard';
 
 function App() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/marioBytes')
+      .then((response) => {
+        setUser(response.data);
+        setLoading(false);
+      })
+  }, [])
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="App">
+    <div className="container">
+      <UserInfoCard user={user} />
     </div>
   );
 }
