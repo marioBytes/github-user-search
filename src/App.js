45f5fcbfc340a24e5b8c96abf4bf5d7aa6c5f670
marioBytes <mario.bytes92@gmail.com>
2022-08-22 16:09:22 -0600

    Remove unused CSS file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import './App.css';
 import UserInfoCard from './components/UserInfoCard';
 import Topbar from './components/Topbar';
 import SearchBar from './components/SearchBar';
