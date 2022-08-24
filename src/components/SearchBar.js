57185695c38d2a1816feab08c7a10590bee9023e
marioBytes <mario.bytes92@gmail.com>
2022-08-24 15:11:29 -0600

    Reconfigure HTML for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,18 +6,22 @@ import IconSearch from './svgs/icon-search';
 const SearchBar = (props) => {
   return (
     <div className="search-bar shadow">
-      <label htmlFor="search">
-        <IconSearch />
-      </label>
-      <input
-        type="text"
-        placeholder="Search GitHub username..."
-        id="search"
-        name="search"
-        style={{ borderRadius: '5px' }}
-        value={props.value}
-        onChange={props.onChange}
-      />
+      <div className="search-bar__input-container">
+        <div className="search-bar__input-container__group">
+          <label htmlFor="search">
+            <IconSearch />
+          </label>
+          <input
+            type="text"
+            placeholder="Search GitHub username..."
+            id="search"
+            name="search"
+            value={props.value}
+            onChange={props.onChange}
+          />
+        </div>
+        {props.errorMessage && <p className="search-bar__input-container__error-message">{props.errorMessage}</p>}
+      </div>
       <button className="search-button" onClick={props.onClick}>
         Search
       </button>
@@ -26,9 +30,10 @@ const SearchBar = (props) => {
 };
 
 SearchBar.propTypes = {
-  value: propTypes.string,
+  errorMessage: propTypes.string,
   onChange: propTypes.func.isRequired,
   onClick: propTypes.func.isRequired,
+  value: propTypes.string,
 };
 
 export default SearchBar;
