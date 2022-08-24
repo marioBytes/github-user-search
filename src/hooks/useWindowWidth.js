03da33e0a140b058ad1e8a4b70fe10fc6c733a83
marioBytes <mario.bytes92@gmail.com>
2022-08-24 08:13:53 -0600

    Create hook to detect screen size

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const useWindowWidth = (size) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setWidth]);
+
+  return width > size;
+};
+
+export default useWindowWidth;
